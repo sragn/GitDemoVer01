62a859327a2005585c8a4554a38a04159b1d31fe
Advita <Advita@Ganesh>
2024-06-21 21:51:06 +0530

    added additional checks

--- src/Test2.java
+++ src/Test2.java
@@ -6,7 +6,7 @@ public class Test2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println("this is demo git as lead");
+		System.out.println("this is demo git as test2 file as client");
 	}
 
 }
